c729808bc4d714b7699860dbfb8fb157657f661e
Anthony Boffman <amarrokalboffman@gmail.com>
2024-09-03 18:08:33 -0700

    Initial commit

--- app/src/main/java/com/example/simpleglucosetracker/MainActivity.kt
+++ app/src/main/java/com/example/simpleglucosetracker/MainActivity.kt
@@ -6,6 +6,15 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val glucoseUnitsDropdown: Spinner = findViewById(R.id.glucoseUnitsDropdown)
+        ArrayAdapter.createFromResource(
+            this, // Context
+            R.array.glucose_units,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            glucoseUnitsDropdown.adapter = adapter
+        }
         setContentView(R.layout.activity_main)
     }
 }
